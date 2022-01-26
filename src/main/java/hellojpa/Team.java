e9f44b4d405932f6ee5d583b29beb9a349dca88f
ooiooc <shinjh1102@naver.com>
2022-01-26 22:49:45 +0900

    양방향 연관관계 설정

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,12 +1,16 @@
 package hellojpa;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @Entity
 public class Team {
 
@@ -16,5 +20,8 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
 }
